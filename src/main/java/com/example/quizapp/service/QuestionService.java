3d3ef8b8eddde5ef6823e5cad00e24da809942e5
Harshavardhan ag <aghar@Mac.ht.home>
2025-11-15 18:48:26 -0500

    add question, to add we need to add in JSON formate so, we use Postman to do, and changed sequence to identity

--- src/main/java/com/example/quizapp/service/QuestionService.java
+++ src/main/java/com/example/quizapp/service/QuestionService.java
@@ -5,6 +5,7 @@ import com.example.quizapp.Question;
 import com.example.quizapp.dao.QuestionDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
@@ -20,5 +21,11 @@ public class QuestionService {
 
     public List<Question> getQuestionByCategory(String category) {
         return questionDao.findByCategory(category);
+    }
+
+    public String addQuestion(@RequestBody Question question) {
+        questionDao.save(question);
+        return "Question added successfully";
+
     }
 }
