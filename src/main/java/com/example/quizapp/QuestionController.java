5e39fc57b4020b23bd3f7923d5f29535949f7f5f
Harshavardhan ag <aghar@MacBook-Air.local>
2025-11-15 15:31:57 -0500

    Added a simple string response in the website to test the controller. Also configured PostgreSQL because it is required for the Spring Boot dependencies in pom.xml.

---
+++ src/main/java/com/example/quizapp/QuestionController.java
@@ -0,0 +1,15 @@
+package com.example.quizapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("Question")
+public class QuestionController {
+    @GetMapping("allQuestions")
+    public String getAllQuestions(){
+        return "Hi, therse are your Question";
+    }
+
+}
