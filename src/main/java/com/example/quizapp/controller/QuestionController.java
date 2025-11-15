8911054c6870f2110b0363e461bbe32ee4e4e8d4
Harshavardhan ag <aghar@Mac.ht.home>
2025-11-15 17:20:42 -0500

    now, changed the database name correctly, and also added controller,dao, service packet and also able to findall data from browser

---
+++ src/main/java/com/example/quizapp/controller/QuestionController.java
@@ -0,0 +1,25 @@
+package com.example.quizapp.controller;
+
+import com.example.quizapp.Question;
+import com.example.quizapp.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("Question")
+public class QuestionController {
+
+    @Autowired
+    QuestionService questionSerice;
+
+
+    @GetMapping("allQuestions")
+    public List<Question> getAllQuestions(){
+        return questionSerice.getAllQuestions();
+    }
+
+}
