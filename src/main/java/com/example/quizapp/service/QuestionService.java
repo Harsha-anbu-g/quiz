ace04a372705544d035761e2c029bec0483601e5
Harshavardhan ag <aghar@MacBook-Air.local>
2025-11-17 23:47:48 -0500

    added the quiz random call code

--- src/main/java/com/example/quizapp/service/QuestionService.java
+++ src/main/java/com/example/quizapp/service/QuestionService.java
@@ -1,13 +1,12 @@
 package com.example.quizapp.service;
 
 
-import com.example.quizapp.Question;
+import com.example.quizapp.model.Question;
 import com.example.quizapp.dao.QuestionDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.ArrayList;
 import java.util.List;
