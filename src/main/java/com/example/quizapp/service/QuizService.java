90de2e306502795c40623e36fa6ca15a769f9152
Harshavardhan ag <aghar@Mac.ht.home>
2025-11-20 18:26:10 -0500

    print the question without correct answer to the user/postman

--- src/main/java/com/example/quizapp/service/QuizService.java
+++ src/main/java/com/example/quizapp/service/QuizService.java
@@ -3,13 +3,16 @@ package com.example.quizapp.service;
 import com.example.quizapp.dao.QuestionDao;
 import com.example.quizapp.dao.QuizDao;
 import com.example.quizapp.model.Question;
+import com.example.quizapp.model.QuestionWrapper;
 import com.example.quizapp.model.Quiz;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class QuizService {
@@ -31,5 +34,16 @@ public class QuizService {
 
         return new ResponseEntity<>("Success", HttpStatus.CREATED);
 
+    }
+
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestion(Integer id) {
+        Optional<Quiz> quiz = quizDao.findById(id);
+        List<Question> questionsFromDB = quiz.get().getQuestions();
+        List<QuestionWrapper> questionForUser = new ArrayList<>();
+        for (Question q : questionsFromDB){
+            QuestionWrapper qw = new QuestionWrapper(q.getId(), q.getQuestionTitle(), q.getOption1(), q.getOption2(), q.getOption3(), q.getOption4() );
+            questionForUser.add(qw);
+        }
+        return new ResponseEntity<>(questionForUser, HttpStatus.OK);
     }
 }
